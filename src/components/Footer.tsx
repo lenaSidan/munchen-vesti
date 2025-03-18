4f0a0de722f485ba45fe43011cd9499fd6e987a1
Elena Sidanova <l.sidan@gmx.de>
2025-03-18 17:46:20 +0100

    MV-42 footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -17,27 +17,31 @@ export default function Footer() {
         </p>
 
         <nav className={styles.nav}>
-          <Link href="/about" className={`${styles.navLink} ${router.pathname === "/about" ? styles.active : ""}`}>
-            {t("footer.about")}
-          </Link>
-          <Link
-            href="/contacts"
-            className={`${styles.navLink} ${router.pathname === "/contacts" ? styles.active : ""}`}
-          >
-            {t("footer.contacts")}
-          </Link>
-          <Link
-            href="/impressum"
-            className={`${styles.navLink} ${router.pathname === "/impressum" ? styles.active : ""}`}
-          >
-            {t("footer.impressum")}
-          </Link>
-          <Link
-            href="/privacy-policy"
-            className={`${styles.navLink} ${router.pathname === "/privacy-policy" ? styles.active : ""}`}
-          >
-            {t("footer.privacy-policy")}
-          </Link>
+          <div className={styles.linkBox}>
+            <Link href="/about" className={`${styles.navLink} ${router.pathname === "/about" ? styles.active : ""}`}>
+              {t("footer.about")}
+            </Link>
+            <Link
+              href="/contacts"
+              className={`${styles.navLink} ${router.pathname === "/contacts" ? styles.active : ""}`}
+            >
+              {t("footer.contacts")}
+            </Link>
+            <Link
+              href="/impressum"
+              className={`${styles.navLink} ${router.pathname === "/impressum" ? styles.active : ""}`}
+            >
+              {t("footer.impressum")}
+            </Link>
+          </div>
+          <div className={styles.policyBox}>
+            <Link
+              href="/privacy-policy"
+              className={`${styles.navLink} ${router.pathname === "/privacy-policy" ? styles.active : ""}`}
+            >
+              {t("footer.privacy-policy")}
+            </Link>
+          </div>
         </nav>
         <p className={styles.copyright}>{t("footer.copyright").replace("{year}", currentYear.toString())}</p>
       </div>
