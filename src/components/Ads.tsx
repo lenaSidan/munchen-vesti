ffb0caca600bfa816fa540452cdacd8e6cc1233d
Elena Sidanova <l.sidan@gmx.de>
2025-03-22 20:38:31 +0100

    MV-48 cocktail week

--- src/components/Ads.tsx
+++ src/components/Ads.tsx
@@ -1,14 +1,10 @@
 import styles from "@/styles/Ads.module.css";
 import TibetanBowls from "./ads/TibetanBowls";
-import SalonRental from "./ads/SalonRental";
-
 
 export default function Ads() {
   return (
     <aside className={styles.announcements}>
       <TibetanBowls />
-      <SalonRental />
-      {/* <LegalServices /> */}
     </aside>
   );
 }
