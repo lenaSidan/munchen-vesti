1b043d71d838afdb0ad0fd5c627d680785df0f64
Elena Sidanova <l.sidan@gmx.de>
2025-03-31 15:56:34 +0200

    MV-61 correct styles

--- src/components/Ads.tsx
+++ src/components/Ads.tsx
@@ -1,9 +1,24 @@
 import styles from "@/styles/Ads.module.css";
 import ViletaJewelry from "./ads/VitaJewelry";
+import Image from "next/image";
+import useTranslation from "@/hooks/useTranslation";
 
 export default function Ads() {
+  const t = useTranslation();
+
   return (
     <aside className={styles.announcements}>
+      <div className={styles.announcementHeader}>
+        <h2 className={styles.announcementTitle}>{t("info")}</h2>
+        <Image
+          src="/icons/info.png"
+          alt="Info"
+          width={20}
+          height={20}
+          className={`${styles.announcementIcon} ${styles.info}`}
+        />
+      </div>
+      <div className={styles.decorativeLine}></div>
       <ViletaJewelry />
     </aside>
   );
