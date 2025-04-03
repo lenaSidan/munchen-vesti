8a97b2b7f4241542f00d36378ed1ff639da15b0a
Elena Sidanova <l.sidan@gmx.de>
2025-04-03 23:09:53 +0200

    add Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { Old_Standard_TT, Merriweather } from "next/font/google";
 import Footer from "@/components/Footer";
 import { useEffect } from "react";
 import CookieConsent from "@/components/CookieConsent";
+import Script from "next/script"; // 👈 импортируем Script
 
 const oldStandard = Old_Standard_TT({ subsets: ["latin", "cyrillic"], weight: "400" });
 const merriweather = Merriweather({ subsets: ["latin", "cyrillic"], weight: ["300", "400"] });
@@ -24,6 +25,24 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <meta name="robots" content="index, follow" />
       </Head>
+
+      {/* Google Analytics Scripts 👇 */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-BRM8FPV3SS"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+      >
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-BRM8FPV3SS');
+        `}
+      </Script>
+
       <Header />
       <main className={oldStandard.className}>
         <Component {...pageProps} />
