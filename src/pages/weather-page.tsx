bf52fabd034574d6a67dfbeb715056de33e5872d
Elena Sidanova <l.sidan@gmx.de>
2025-04-16 18:25:09 +0200

    correct Weather

--- src/pages/weather-page.tsx
+++ src/pages/weather-page.tsx
@@ -22,24 +22,31 @@ export default function WeatherPage({ weather }: Props) {
       <ul className={styles.weatherList}>
         {weather.time.map((date, i) => (
           <li key={date} className={styles.weatherItem}>
-            <span className={styles.weatherDate}>
-              {new Date(date).toLocaleDateString(locale, {
-                weekday: "short",
-                day: "numeric",
-                month: "long",
-              })}
-            </span>
-            <span className={styles.weatherDetails}>
-              {weather.temperature_2m_min[i]}°C – {weather.temperature_2m_max[i]}°C, {t("weather.precip")}{" "}
-              {weather.precipitation_sum[i]} мм
+            <div className={styles.weatherHeader}>
+              <span className={styles.weatherDate}>
+                {new Date(date).toLocaleDateString(locale, {
+                  weekday: "short",
+                  day: "numeric",
+                  month: "long",
+                })}
+              </span>
               <Image
                 src={getWeatherIcon(weather.weathercode[i])}
                 alt="weather icon"
                 className={styles.weatherIcon}
-                width={30}
-                height={30}
+                width={32}
+                height={32}
               />
-            </span>
+            </div>
+            <div className={styles.weatherDetails}>
+              <div className={styles.temp}>
+                <span className={styles.min}>↓ {weather.temperature_2m_min[i]}°C</span>
+                <span className={styles.max}>↑ {weather.temperature_2m_max[i]}°C</span>
+              </div>
+              <span className={styles.precip}>
+                {t("weather.precip")}: {weather.precipitation_sum[i]} мм
+              </span>
+            </div>
           </li>
         ))}
       </ul>
@@ -51,6 +58,6 @@ export const getStaticProps: GetStaticProps = async () => {
   const weather = await getWeatherForecast();
   return {
     props: { weather },
-    revalidate: 43200, // обновляется каждые 12 часов
+    revalidate: 43200,
   };
 };
